37e377897340067fb8b9858f4959e2af67fde9f4
Margarita Popova <margarita.kistanova@gmail.com>
2020-11-23 14:09:17 +0300

    tests upd

--- ads/tests.py
+++ ads/tests.py
@@ -39,7 +39,6 @@ class TestListView(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='12345678qwerty')
-        self.ad = AdFactory.build(owner=self.user)
         self.response = self.client.get(reverse('ads:all'))
 
     def test_index(self):
@@ -48,21 +47,6 @@ class TestListView(TestCase):
         self.assertTemplateUsed(self.response, 'ads/ad_list.html')
         self.assertContains(self.response, 'There are no ads')
         self.assertQuerysetEqual(self.response.context['ad_list'], [])
-
-    def test_post(self):
-        print(f'testing {self.__class__.__name__}: {self.test_post.__name__}')
-
-        self.client.login(username='testuser', password='12345678qwerty')
-        url = reverse('ads:ad_create')
-        self.client.post(url,
-                         {'title': self.ad.title, 'text': self.ad.text, 'price': self.ad.price})
-        response = self.client.get(reverse('ads:all'))
-
-        with self.subTest():
-            self.assertContains(response, 'class="card')
-            self.assertContains(response, 'btn-success')
-            self.assertContains(response, self.ad.title)
-            self.assertQuerysetEqual(response.context['ad_list'], ['<Ad: Phone>'])
 
     def test_favs(self):
         print(f'testing {self.__class__.__name__}: {self.test_favs.__name__}')
@@ -78,14 +62,35 @@ class TestListView(TestCase):
         self.assertContains(response, 'Logout')
         self.client.logout()
 
+
+class TestCreateView(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345678qwerty')
+        self.ad = AdFactory.build(owner=self.user)
+        self.url = reverse('ads:ad_create')
+
+    def test_post(self):
+        print(f'testing {self.__class__.__name__}: {self.test_post.__name__}')
+
+        self.client.login(username='testuser', password='12345678qwerty')
+        self.client.post(self.url,
+                         {'title': self.ad.title, 'text': self.ad.text, 'price': self.ad.price})
+        response = self.client.get(reverse('ads:all'))
+
+        with self.subTest():
+            self.assertContains(response, 'class="card')
+            self.assertContains(response, 'btn-success')
+            self.assertContains(response, self.ad.title)
+            self.assertQuerysetEqual(response.context['ad_list'], ['<Ad: Phone>'])
+
     def test_permissions(self):
         print(f'testing {self.__class__.__name__}: {self.test_permissions.__name__}')
 
-        url = reverse('ads:ad_create')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302)
         self.client.login(username='testuser', password='12345678qwerty')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-
 
