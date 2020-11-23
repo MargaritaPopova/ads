84fe2c01febc5e5cc0b18fbf827222158e8b2a53
Margarita Popova <margarita.kistanova@gmail.com>
2020-11-23 13:50:05 +0300

    Deleted unused apps

--- unesco/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from unesco.models import Site, Category, Region, States, ISO
-
-admin.site.register(Site)
-admin.site.register(Category)
-admin.site.register(Region)
-admin.site.register(States)
-admin.site.register(ISO)
--- unesco/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UnescoConfig(AppConfig):
-    name = 'unesco'
