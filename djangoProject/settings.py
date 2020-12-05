4f34cd8f90d1bb6661663175a63f13abfec14e7b
Margarita Popova <margarita.kistanova@gmail.com>
2020-12-05 21:21:58 +0300

    actions tests clean

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -1,20 +1,17 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import django
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Used for a default title
 APP_NAME = 'Ads'
 
-SECRET_KEY = os.environ.get("SECRET_KEY") or "g$iqqu&*mw4_sg3(#ld0sqaalxebel&168^yj%i&sgrw(fmn@w"
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1'[::1]]
+ALLOWED_HOSTS = ['*']
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
@@ -76,16 +73,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
-DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE") or 'django.db.backends.postgresql_psycopg2',
-        "NAME": os.environ.get("SQL_DATABASE") or 'ads',
-        "USER": os.environ.get("SQL_USER") or 'postgres',
-        "PASSWORD": os.environ.get("SQL_PASSWORD") or 'password',
-        "HOST": os.environ.get("SQL_HOST") or 'db',
-        "PORT": os.environ.get("SQL_PORT") or 5432,
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'ads',
+           'USER': 'postgres',
+           'PASSWORD': 'password',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ.get("SQL_ENGINE"),
+            "NAME": os.environ.get("SQL_DATABASE"),
+            "USER": os.environ.get("SQL_USER"),
+            "PASSWORD": os.environ.get("SQL_PASSWORD"),
+            "HOST": os.environ.get("SQL_HOST"),
+            "PORT": os.environ.get("SQL_PORT"),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
