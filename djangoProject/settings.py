cffaa493896aff27bc5a1dd382e5b3387596c6fe
Margarita Popova <margarita.kistanova@gmail.com>
2020-12-15 13:19:36 +0300

    Added files and changes required for deployment to heroku

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -9,7 +9,7 @@ APP_NAME = 'Ads'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
@@ -145,3 +145,7 @@ LOGOUT_REDIRECT_URL = '/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
