b8b18ff6e421b43262184ae72a0b6ad450ad72ed
Margarita Popova <margarita.kistanova@gmail.com>
2020-12-15 13:40:05 +0300

    Secret key f-ing exposed

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Used for a default title
 APP_NAME = 'Ads'
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "g$iqqu&*mw4_sg3(#ld0sqaalxebel&168^yj%i&sgrw(fmn@w")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') != 'False'
