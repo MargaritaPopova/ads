205ae19e3d9113cd53f0d03064c2187e0e996ba3
Margarita Popova <margarita.kistanova@gmail.com>
2020-12-16 18:36:02 +0300

    staticfiles

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -13,9 +13,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
