41e45aed3576ea4f37c27e7eaa1e850c3141f502
Margarita Popova <margarita.kistanova@gmail.com>
2021-01-05 23:36:09 +0600

    profile edit page fixed

--- core/models.py
+++ core/models.py
@@ -4,8 +4,8 @@ from phone_field import PhoneField
 
 
 class UserProfile(models.Model):
-    home_address = models.CharField(max_length=120, null=True, default='')
-    phone_number = PhoneField(blank=True, help_text='Contact phone number')
+    home_address = models.CharField(max_length=120, null=True, default='', blank=True)
+    phone_number = PhoneField(blank=False, help_text='Contact phone number')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(null=True, blank=True)
     birthdate = models.DateField(null=True, verbose_name='Date of birth', blank=True)
