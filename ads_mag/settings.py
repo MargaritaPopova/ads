5180d91e9fb2b947fbe3a08ed5ca31f38d604b6f
Margarita Popova <margarita.kistanova@gmail.com>
2021-01-07 22:29:38 +0600

    chat w/ channels fixes

--- ads_mag/settings.py
+++ ads_mag/settings.py
@@ -79,7 +79,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [(os.environ.get('REDIS_HOST', 'localhost'), 6379)],
         },
     },
 }
